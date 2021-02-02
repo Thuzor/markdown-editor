84f4669d83f0ce1e3f16f148f2258c4984912ca2
Adam Thewlis <adam.thewlis@theroundhouse.co.uk>
2021-02-02 13:42:14 +0000

    Create markdown editor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import ReactMarkdown from 'react-markdown';
+
 export default function App() {
+
+    const [markdown, setMarkdown] = useState('');
+
+    const handleChange = (e) => {
+        setMarkdown(e.target.value)
+    }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea value={markdown} onChange={handleChange} />
 
-      <div className="preview" />
+      <ReactMarkdown className="preview" source={markdown} />
     </div>
   );
 }
